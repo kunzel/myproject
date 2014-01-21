14bba5ac0629e3bf6951cc42f76c816a2bad6a49
Lars Kunze <kula78@gmail.com>
2014-01-21 14:35:56 +0000

    read name from commandline

--- MyClass.java
+++ MyClass.java
@@ -1,9 +1,20 @@
+import java.io.*;
 
 public class MyClass{
 
 	public static void main(String args[]){
 		
-		System.out.println("Hello World");
+		System.out.println("Hello, enter your name:");
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String name = null;
+		try {
+             		name = br.readLine();
+      		} catch (IOException ioe) {
+         		System.out.println("IO error trying to read your name!");
+         		System.exit(1);
+     		}
+		System.out.println("Hello " + name);
 
 	}
 }
